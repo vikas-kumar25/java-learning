c0349dceaba6c1eed89fff503bcc6d189d7ea183
vikas-kumar25 <vikaskumar25it@gmail.com>
2026-05-03 19:09:51 +0530

    Added If-Else decision making program

---
+++ src/basics/IfElse.java
@@ -0,0 +1,23 @@
+package basics;
+
+/**********************************************
+ Developer Name: Vikash Kumar
+ Created on: 03-05-2026 19:06
+ Project Name: java-learning ${
+ /**********************************************/
+public class IfElse {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 70) {
+            System.out.println("Grade B");
+        } else if (marks >= 50) {
+            System.out.println("Grade C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
