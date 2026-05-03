d5d6dd11d10e993861accca0cfd209754d86884a
vikas-kumar25 <vikaskumar25it@gmail.com>
2026-05-03 17:43:36 +0530

    Initial commit: Added Variables program

---
+++ src/basics/Variables.java
@@ -0,0 +1,16 @@
+package basics;
+
+/**********************************************
+ Developer Name: Vikas
+ Created on: 03-05-2026 17:16
+ Project Name: java-learning ${
+ /**********************************************/
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.5;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
