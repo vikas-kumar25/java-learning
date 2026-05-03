d0b1291adac1567fff31e45a677f334872be5e8d
vikas-kumar25 <vikaskumar25it@gmail.com>
2026-05-03 19:20:01 +0530

    Added loops concept

---
+++ src/basics/Loops.java
@@ -0,0 +1,25 @@
+package basics;
+
+/**********************************************
+ Developer Name: Vikas
+ Created on: 03-05-2026 19:15
+ Project Name: java-learning ${
+ /**********************************************/
+public class Loops {
+    public static void main(String[] args) {
+
+        // For loop
+        System.out.println("For Loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // While loop
+        System.out.println("While Loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
