51d2b4c66ff69ef6dc36dc79336d949978aaa734
vikas-kumar25 <vikaskumar25it@gmail.com>
2026-05-11 15:21:56 +0530

    Added shallow copy constructor example

---
+++ src/oops/ShallowCopyConstructor.java
@@ -0,0 +1,36 @@
+package oops;
+
+/**********************************************
+ Developer Name: Vikas
+ Created on: 11-05-2026 15:04
+ Project Name: java-learning ${
+ /**********************************************/
+
+public class ShallowCopyConstructor {
+
+    int roll;
+    int age;
+    String name;
+    String gender;
+
+
+
+    // Normal Constructor
+    public ShallowCopyConstructor(int roll, int age, String name, String gender) {
+
+        this.roll = roll;
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public static void main(String[] args) {
+
+        ShallowCopyConstructor s1 = new ShallowCopyConstructor(101, 21, "Anshuman", "Male");
+
+        // shallow copy
+        ShallowCopyConstructor s2 = s1;
+
+        System.out.println(s1 == s2);
+    }
+}
